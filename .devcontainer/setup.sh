e6227fd0569175eaf67ad35b846b763895143e62
heerabete <125117306+heerabete@users.noreply.github.com>
2025-04-12 10:32:32 +0000

    Initial full repo push 🚀

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -17,4 +17,11 @@ ulimit -n 1048576
 
 cd "/workspaces/C93"
 
+wget -q https://raw.githubusercontent.com/naksh-07/Automate/refs/heads/main/thorium.sh && chmod +x thorium.sh && ./thorium.sh
+
+curl -sSLO https://raw.githubusercontent.com/naksh-07/Automate/refs/heads/main/gaianet.sh && bash gaianet.sh
+curl -sSLO https://raw.githubusercontent.com/naksh-07/Automate/refs/heads/main/ognode.sh && bash ognode.sh
+curl -sSLO https://raw.githubusercontent.com/naksh-07/Automate/refs/heads/main/Gbot.sh && bash Gbot.sh
+
+
 echo "✅ All Done Bhai! Ultra OP Container READY 🚀"
